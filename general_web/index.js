6615c90fb886f76018cb2e2d98963bc5972c702b
tkakrdpahd <89321156+tkakrdpahd@users.noreply.github.com>
2024-01-25 11:09:49 -0500

    update logo and scripts
    update logo and scripts

--- general_web/index.js
+++ general_web/index.js
@@ -3,8 +3,11 @@
 var link = 'https://notice.libramentum.co.kr';
 var link_01 = "//html/about_us.html: 8080";
 
-function hrefLink() {
+function hrefLink(page) {
     location.href = link;
 }
 
+function logoLink() {
+
+}
 // node.js redirection to data server
