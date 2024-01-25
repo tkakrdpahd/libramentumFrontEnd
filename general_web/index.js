f7f247aa58693810c11eb1c224936dbca496fc64
tkakrdpahd <89321156+tkakrdpahd@users.noreply.github.com>
2024-01-25 13:39:48 -0500

    ui update
    size and mobile device

--- general_web/index.js
+++ general_web/index.js
@@ -1,10 +1,25 @@
-// link redirection will be chage
-
-var link = 'https://notice.libramentum.co.kr';
-var link_01 = "//html/about_us.html: 8080";
+// link redirection
 
 function hrefLink(page) {
-    location.href = link;
+    switch(page) {
+        case 'ab':
+            window.location.href = './html/about_us.html';
+            break;
+        case 'an':
+            window.location.href = 'https://notice.libramentum.co.kr';
+            break;
+        case 'cr':
+            window.location.href = 'https://creators.libramentum.co.kr';
+            break;
+        case 'ca':
+            window.location.href = 'https://carrier.libramentum.co.kr';
+            break;
+        case 'sh':
+            window.location.href = 'https://shop.libramentum.co.kr';
+            break;
+        default:
+            break;
+    }
 }
 
 function logoLink() {
