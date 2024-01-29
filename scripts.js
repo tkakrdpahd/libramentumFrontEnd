7cc6451e761f92b2c8d5b7a569d20c006946e487
tkakrdpahd <89321156+tkakrdpahd@users.noreply.github.com>
2024-01-29 11:10:11 -0500

    update
    general page update

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var ids = ['index', 'about', 'mission', 'contact_us'];
+    var ids = ['index', 'about', 'CEO_profile', 'mission', 'contact_us', 'notice', 'IR'];
     ids.forEach(function(id) {
         var element = document.getElementById(id);
         if (element) {
@@ -14,17 +14,26 @@ document.addEventListener('DOMContentLoaded', function () {
 function hrefLink(page) {
     switch(page) {
         case 'index':
-            window.location.href = '../index.html'; //http://www.libramentum.co.kr/
+            window.location.href = 'http://www.libramentum.co.kr/index.html';
             break;
         case 'about':
-            window.location.href = ''; // ./html/about.html
+            window.location.href = 'http://www.libramentum.co.kr/html/about.html';
             break;
+        case 'CEO_profile':
+                window.location.href = '';
+                break;
         case 'mission':
-            window.location.href = ''; // ./html/about_us.html
+            window.location.href = 'http://www.libramentum.co.kr/html/about.html';
             break;
         case 'contact_us':
-            window.location.href = ''; // ./html/contact_us.html
+            window.location.href = 'http://www.libramentum.co.kr/html/contact_us.html';
             break;
+        case 'notice':
+            window.location.href = 'http://www.libramentum.co.kr/html/notice.html';
+            break;
+        case 'IR':
+            window.location.href = 'http://www.libramentum.co.kr/html/ir.html';
+        break;
     }
 }
 
