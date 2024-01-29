a7e0cc3d60878e5a34c5713325afac7b68e5a1da
tkakrdpahd <89321156+tkakrdpahd@users.noreply.github.com>
2024-01-29 11:38:15 -0500

    update
    link update

--- scripts.js
+++ scripts.js
@@ -14,25 +14,25 @@ document.addEventListener('DOMContentLoaded', function () {
 function hrefLink(page) {
     switch(page) {
         case 'index':
-            window.location.href = 'http://www.libramentum.co.kr/index.html';
+            window.location.href = '../index.html';
             break;
         case 'about':
-            window.location.href = 'http://www.libramentum.co.kr/html/about.html';
+            window.location.href = './html/about.html';
             break;
         case 'CEO_profile':
                 window.location.href = '';
                 break;
         case 'mission':
-            window.location.href = 'http://www.libramentum.co.kr/html/about.html';
+            window.location.href = './html/about.html';
             break;
         case 'contact_us':
-            window.location.href = 'http://www.libramentum.co.kr/html/contact_us.html';
+            window.location.href = './html/contact_us.html';
             break;
         case 'notice':
-            window.location.href = 'http://www.libramentum.co.kr/html/notice.html';
+            window.location.href = './html/notice.html';
             break;
         case 'IR':
-            window.location.href = 'http://www.libramentum.co.kr/html/ir.html';
+            window.location.href = './html/ir.html';
         break;
     }
 }
